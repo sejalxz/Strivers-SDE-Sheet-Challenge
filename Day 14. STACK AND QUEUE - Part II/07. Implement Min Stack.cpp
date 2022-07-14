75be3d54bad7e4be049485cf4577247703e36c72
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-14 19:22:03 +0530

    Update 07. Implement Min Stack.cpp

--- Day 14. STACK AND QUEUE - Part II/07. Implement Min Stack.cpp
+++ Day 14. STACK AND QUEUE - Part II/07. Implement Min Stack.cpp
@@ -38,3 +38,66 @@ public:
     
 };
 
+
+// OPTIMAL
+// TC : O(1)
+// SC : O(N)
+
+class MinStack {
+private:
+    stack<long long> st;
+    long long minVal;
+public:
+    MinStack() {
+        minVal = INT_MAX;
+        while(!st.empty())
+        {
+            st.pop();
+        }
+    }
+    
+    void push(int val) {
+        if(st.empty()) {
+              minVal = val;
+              st.push((long long)val);
+        } 
+        else {
+            if(val < minVal){
+                st.push(2 * (long long)val - minVal);
+                minVal = val;
+            } 
+            else{
+                st.push(val);
+            }
+        }
+  }
+    
+    void pop() {
+        if(st.empty())
+            return;
+        
+        if(st.top() < minVal)
+        {
+            minVal = 2*minVal - st.top();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        if(st.empty())
+            return -1;
+        
+        if(st.top() < minVal){
+            return minVal;
+        }
+        return st.top();
+    }
+    
+    int getMin() {
+        if(st.empty())
+            return -1;
+        return minVal;
+    }
+};
+
+
