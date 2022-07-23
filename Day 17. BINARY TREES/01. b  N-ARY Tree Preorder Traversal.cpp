170571bd89dc044fe8dc79f68e2c54ead0f5ccd0
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-23 14:54:35 +0530

    Update 01. b  N-ARY Tree Preorder Traversal.cpp

--- Day 17. BINARY TREES/01. b  N-ARY Tree Preorder Traversal.cpp
+++ Day 17. BINARY TREES/01. b  N-ARY Tree Preorder Traversal.cpp
@@ -27,3 +27,38 @@ public:
         }
     }
 };
+
+
+// Iterative Preorder Traversal of N-ary Tree
+
+// TC : O(N)
+// SC : O(H)
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int>nodes;
+        
+        if(!root)
+            return nodes;
+        
+        stack<Node*>st;
+        
+        st.push(root);
+        
+        while(!st.empty())
+        {
+            Node* currNode = st.top();
+            st.pop();
+            
+            nodes.push_back(currNode->val);
+            
+            for(int i = currNode->children.size()-1; i>=0 ; i--)
+            {
+                st.push(currNode->children[i]);
+            }
+        }
+        
+        return nodes;
+    }
+};
