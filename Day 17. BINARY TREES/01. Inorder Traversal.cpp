caf1aaf6ce6894781fad2f94fff5980507b2dd1f
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-23 09:21:48 +0530

    Create 01. Inorder Traversal.cpp

---
+++ Day 17. BINARY TREES/01. Inorder Traversal.cpp
@@ -0,0 +1,24 @@
+// Recursive
+
+//TC : O(N)
+//SC : O(H) -> RecStack
+
+class Solution {    
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>nodes;
+        preorder(root, nodes);
+        return nodes;
+    }
+    
+    void preorder(TreeNode* root, vector<int> &nodes)
+    {
+        if(root == NULL)
+            return;
+        
+        nodes.push_back(root->val);
+        preorder(root->left, nodes);
+        preorder(root->right, nodes);
+    }
+};
+
