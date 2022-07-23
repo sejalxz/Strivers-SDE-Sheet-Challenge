4dd3bd42a0ab3fa9abff0e2a3f028afb11d60615
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-23 16:24:01 +0530

    Update 02. Inorder Traversal.cpp

--- Day 17. BINARY TREES/02. Inorder Traversal.cpp
+++ Day 17. BINARY TREES/02. Inorder Traversal.cpp
@@ -21,3 +21,52 @@ private:
         inorderT(root->right, nodes);
     }
 };
+
+
+// Iterative BT Postorder Traversal
+
+// TC : O(N)
+// SC : O(H) (Auxilary Stack)
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>nodes;
+        stack<TreeNode*>st;
+        
+        if(!root)
+            return nodes;
+        
+        st.push(root);
+        addLeftSubTree(root, st);
+        
+        while(!st.empty())
+        {
+            TreeNode* currNode = st.top();
+            st.pop();
+                             
+            nodes.push_back(currNode->val);
+            
+            if(currNode->right)
+            {
+                st.push(currNode->right);
+                addLeftSubTree(currNode->right,st);
+            }        
+        }
+        return nodes;
+    }
+    
+    void addLeftSubTree(TreeNode* root, stack<TreeNode*>&st)
+    {
+        if(root->left){
+            TreeNode* currentNode = root->left;
+            
+            while(currentNode)
+            {
+                st.push(currentNode);
+                currentNode = currentNode->left;
+            }
+        }
+        
+    }
+};
