98c8652e7af2ef6b87ca4453e8c549a75fd5924f
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-25 11:48:31 +0530

    Create 02. c Min Depth of a binary Tree.cpp

---
+++ Day 18. BINARY TREES - Part II/02. c Min Depth of a binary Tree.cpp
@@ -0,0 +1,17 @@
+// Recursive 
+// TC : O(N)
+// SC : O(N)
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        
+        return (left == 0 || right == 0) ? left + right + 1: 1 + min(left, right);
+    }
+};
+
