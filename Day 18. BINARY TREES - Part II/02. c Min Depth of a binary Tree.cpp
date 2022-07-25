7821f8f6bb53cb0f71cacbabeece8abcb84e4128
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-25 13:18:22 +0530

    Update 02. c Min Depth of a binary Tree.cpp

--- Day 18. BINARY TREES - Part II/02. c Min Depth of a binary Tree.cpp
+++ Day 18. BINARY TREES - Part II/02. c Min Depth of a binary Tree.cpp
@@ -15,3 +15,42 @@ public:
     }
 };
 
+// Iterative 
+// TC : O(N)
+// SC : O(N)
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        int height = 0;
+        
+        while(!q.empty())
+        {
+            height++;
+            int size = q.size();
+            
+            while(size--)
+            {
+                TreeNode* currentNode = q.front();
+                q.pop();
+                
+                if(currentNode->left)
+                    q.push(currentNode->left);
+                
+                if(currentNode->right)
+                    q.push(currentNode->right);
+                
+                if(currentNode->left == NULL && currentNode->right ==NULL)
+                    return height;
+            }
+        }
+        
+        return -1;
+    }
+};
