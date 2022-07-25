34f70f134ce9c523795f84208298a8877336c9ad
Sejal Dahake <71432400+sejalxz@users.noreply.github.com>
2022-07-25 10:18:51 +0530

    Update 02. Height of a Binary Tree.cpp

--- Day 18. BINARY TREES - Part II/02. Height of a Binary Tree.cpp
+++ Day 18. BINARY TREES - Part II/02. Height of a Binary Tree.cpp
@@ -14,3 +14,39 @@ public:
         return 1 + max(left , right);
     }
 };
+
+// Iterative 
+// TC : O(N)
+// SC : O(N) Stack
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        if(!root)
+            return depth;
+        
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            depth++;
+            int nodes = q.size();
+            
+            while(nodes--)
+            {
+                TreeNode* currNode = q.front();
+                q.pop();
+                
+                if(currNode->left)
+                    q.push(currNode->left);
+                
+                if(currNode->right)
+                    q.push(currNode->right);
+            }
+        }
+        
+        return depth;
+    }
+};
